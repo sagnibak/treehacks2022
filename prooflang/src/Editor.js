797ba66623f7dfb49125bafdb86b6c0e607546e2
Eric Wang <erwang01@gmail.com>
2022-02-19 20:04:16 -0800

    pre-eject commit

--- prooflang/src/Editor.js
+++ prooflang/src/Editor.js
@@ -1,10 +1,14 @@
 import './Editor.css';
 import AceEditor from 'react-ace';
+import * as wasm from 'prooflang-wasm';
 
 function Editor() {
     let handleInput = (value) => {
         console.log(value);
     }
+
+
+    wasm.greet();
 
     return (
         <AceEditor mode="java" theme="github" name="editor" onChange={handleInput} />
