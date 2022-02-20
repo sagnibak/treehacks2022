bc9613447303d0f93709d7dab5dfc62e9d335c7b
Eric Wang <erwang01@gmail.com>
2022-02-19 23:54:58 -0800

    Display types proposal

--- prooflang/src/Display.js
+++ prooflang/src/Display.js
@@ -2,10 +2,21 @@ import './Display.css';
 
 function Display({wasm, env, types}) {
     window.types = types;
+    window.env = env;
     return (
         <div className="Display">
             <h1>Display</h1>
             <p>types: {types}</p>
+            <div className='type-list'> 
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
