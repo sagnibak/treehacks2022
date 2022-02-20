0285632103e2a57222c1b95572b49b216e238c81
Eric Wang <erwang01@gmail.com>
2022-02-20 00:00:04 -0800

    more display types proposal

--- prooflang/src/Display.js
+++ prooflang/src/Display.js
@@ -7,12 +7,60 @@ function Display({wasm, env, types}) {
         <div className="Display">
             <h1>Display</h1>
             <p>types: {types}</p>
+            <p> Types </p>
             <div className='type-list'> 
                 <div className='type'>
                     Bool
                     <div className='unit-selection'>
                         <span className='unit'>True</span>
                         <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Bool
+                    <div className='unit-selection'>
+                        <span className='unit'>True</span>
+                        <span className='unit'>False</span>
+                    </div>
+                </div>
+
+                <div className='type'>
+                    Singleton
+                    <div className='unit-selection'>
+                        <span className='unit'>One</span>
                     </div>
                 </div>
             </div>
