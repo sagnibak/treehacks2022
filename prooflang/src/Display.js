0af59a6693af3c42d1a3bcd42e577465335561e7
Eric Wang <erwang01@gmail.com>
2022-02-20 00:26:27 -0800

    random color generator

--- prooflang/src/Display.js
+++ prooflang/src/Display.js
@@ -1,15 +1,25 @@
 import './Display.css';
+import randomColor from 'randomcolor';
+import React from 'react';
 
 function Display({wasm, env, types}) {
+    const [colorMap, setColorMap] = React.useState({null: '#000000'});
     window.types = types;
     window.env = env;
+    
+    if (!("Bool" in colorMap)) {
+        setColorMap({...colorMap, "Bool":randomColor()});
+    }
+    if (!("Singleton" in colorMap)) {
+        setColorMap({...colorMap, "Singleton":randomColor()});
+    }
     return (
         <div className="Display">
             <h1>Display</h1>
             <p>types: {types}</p>
             <p> Types </p>
             <div className='type-list'> 
-                <div className='type'>
+                <div className='type' style={{backgroundColor:colorMap["Bool"]}}>
                     <span className='type-name'>Bool</span>  
                     <div className='unit-selection'>
                         <span className='unit'>True</span>
@@ -57,7 +67,7 @@ function Display({wasm, env, types}) {
                     </div>
                 </div>
 
-                <div className='type'>
+                <div className='type' style={{backgroundColor:colorMap["Singleton"]}}>
                     <span>Singleton</span>  
                     <div className='unit-selection'>
                         <span className='unit'>One</span>
